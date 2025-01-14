27654bff3ddc447def89292e819e89cb2a3f74e4
Yogesh Palve <dnyyogesh@gmail.com>
2025-01-14 23:37:07 +0530

    routes created

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import "colors";
 import dotenv from "dotenv";
 import connectDb from "./database/db.js";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config({});
 const app = express();
@@ -12,6 +13,21 @@ app.use(express.json());
 // call database connection
 connectDb();
 
+app.use("/api/v1/user", userRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    success: true,
+    message: "Web server started successfully",
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server listen at http://localhost:${port}`.bgWhite);
+});
+
+// global catch
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(404).send("Error");
 });
