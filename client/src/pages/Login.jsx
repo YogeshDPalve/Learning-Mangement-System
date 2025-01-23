5eabd8bf5adc4ad8e8d9bfdfdec281ef0103d87f
Yogesh Palve <dnyyogesh@gmail.com>
2025-01-23 23:22:11 +0530

    minor bug fixing

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -18,7 +18,6 @@ import {
 import { Loader2 } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 
 const Login = () => {
--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
