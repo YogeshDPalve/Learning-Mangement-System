776bd5dcfb387f78a7b2fada723bc1f4f4c825e3
Yogesh Palve <dnyyogesh@gmail.com>
2025-01-23 23:40:32 +0530

    course schema and controller created

---
+++ server/controllers/course.controller.js
@@ -0,0 +1,33 @@
+import { courseModel } from "../models/course.model";
+
+const createCourseController = async (req, res) => {
+  try {
+    const { courseTitle, category } = req.body;
+    if (!courseTitle || !category) {
+      return res.status(400).send({
+        success: false,
+        message: "Course title and category is required.",
+      });
+    }
+
+    const course = await courseModel.create({
+      courseTitle,
+      category,
+      creator: req.id,
+    });
+    return res.status(201).send({
+      success: true,
+      course,
+      message: "Course created successfully.",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      success: false,
+      message: "Failed to create course.",
+      error,
+    });
+  }
+};
+
+export { createCourseController };
