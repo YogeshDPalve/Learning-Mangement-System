ea7b98c415f79f2abe19e78827557fe11c2865f3
Yogesh Palve <dnyyogesh@gmail.com>
2025-01-27 14:45:00 +0530

    lecture routes added

--- server/routes/course.routes.js
+++ server/routes/course.routes.js
@@ -3,9 +3,12 @@ import {
   createCourseController,
   createLectureController,
   editCourseController,
+  editLectureController,
   getCourseByIdController,
   getCreatorCoursesController,
+  getLectureByIdController,
   getLectureController,
+  removeLectureController,
 } from "../controllers/course.controller.js";
 import authMiddleware from "../middlewares/isAuthenticated.js";
 import upload from "../utils/multer.js";
@@ -24,5 +27,21 @@ router.get("/:courseId", authMiddleware, getCourseByIdController);
 
 router.post("/:courseId/lectures", authMiddleware, createLectureController);
 router.get("/:courseId/lectures", authMiddleware, getLectureController);
+
+router.post(
+  "/:courseId/lectures/:lectureId",
+  authMiddleware,
+  editLectureController
+);
+router.delete(
+  "/:courseId/lectures/:lectureId",
+  authMiddleware,
+  removeLectureController
+);
+router.get(
+  "/:courseId/lectures/:lectureId",
+  authMiddleware,
+  getLectureByIdController
+);
 
 export default router;
