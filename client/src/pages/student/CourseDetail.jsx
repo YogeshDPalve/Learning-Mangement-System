338a342a85c5cd75efe111d41bb46fcef1675dce
Yogesh Palve <dnyyogesh@gmail.com>
2025-01-30 23:17:51 +0530

    course details page created to show details of course

---
+++ client/src/pages/student/CourseDetail.jsx
@@ -0,0 +1,82 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { BadgeInfo, Lock, PlayCircle } from "lucide-react";
+import React from "react";
+
+const CourseDetail = () => {
+  const purchasedcourse = true;
+  return (
+    <div className="mt-20 space-y-5">
+      <div className="bg-[#2D2F31] text-white">
+        <div className="max-w-7xl mx-auto py-8 px-4 md:px-8 flex flex-col gap-2">
+          <h1 className="font-bold text-2xl md:text-3xl">Course Title</h1>
+          <p className="text-base md:text-lg">Course sub title</p>
+          <p>
+            created By{" "}
+            <span className="text-[#C0C4FC] underline italic">
+              Hannah Baker
+            </span>
+          </p>
+          <div className="flex items-center gap-2 text-sm">
+            <BadgeInfo size={16} />
+            <p>Last updated 31-01-2025</p>
+          </div>
+          <p>Student Enrolled: 20</p>
+        </div>
+      </div>
+      <div className="max-w-7xl mx-auto my-5 px-4 md:px-8 flex flex-col lg:flex-row justify-between gap-10">
+        <div className="w-full lg:w-1/2 space-y-5">
+          <h1 className="font-bold text-xl md:text-2xl">Description</h1>
+          <p className="text-sm">
+            1 import from 'vite'; I have imported this module by mistake and
+            that's why it was giving me the error. You can remove the file(You
+            may import another type of file. So it is better to type
+            ctrl+shift+f and search "from 'vite'".Then you can delete the
+            redundant imports.)
+          </p>
+          <Card>
+            <CardHeader>
+              <CardTitle>Course Content</CardTitle>
+              <CardDescription>4 lectures</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-3">
+              {[1, 2, 3].map((_, idx) => (
+                <div key={idx} className="flex items-center gap-3 text-sm">
+                  <span>
+                    {true ? <PlayCircle size={14} /> : <Lock size={14} />}
+                  </span>
+                  <span>Lecture Heading</span>
+                </div>
+              ))}
+            </CardContent>
+          </Card>
+        </div>
+        <div className="w-full lg:w-1/3 ">
+          <Card>
+            <CardContent className="p-4 flex flex-col ">
+              <div className="w-full aspect-video mb-4">react video player</div>
+              <h1>Lecture title</h1>
+              <Separator className="my-2" />
+              <h1 className="text-lg md:text-xl font-semibold">Course Price</h1>
+            </CardContent>
+            <CardFooter className="flex justify-center p-4">
+              <Button variant="" className="w-full"	>
+                {purchasedcourse ? "Continue course" : "Purchase Course"}
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CourseDetail;
