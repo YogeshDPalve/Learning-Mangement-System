644c4e8b166fdf038d2f7ab1d84701d029b1692e
Yogesh Palve <dnyyogesh@gmail.com>
2025-01-31 08:04:09 +0530

    minor addition in course details page

--- client/src/pages/student/CourseDetail.jsx
+++ client/src/pages/student/CourseDetail.jsx
@@ -1,3 +1,4 @@
+import BuyCourseButton from "@/components/BuyCourseButton";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -7,14 +8,14 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
+import { Separator } from "@/components/ui/separator"; 
 import { BadgeInfo, Lock, PlayCircle } from "lucide-react";
 import React from "react";
 
 const CourseDetail = () => {
-  const purchasedcourse = true;
+  const purchasedcourse = false;
   return (
-    <div className="mt-20 space-y-5">
+    <div className="mt-16 space-y-5">
       <div className="bg-[#2D2F31] text-white">
         <div className="max-w-7xl mx-auto py-8 px-4 md:px-8 flex flex-col gap-2">
           <h1 className="font-bold text-2xl md:text-3xl">Course Title</h1>
@@ -68,9 +69,11 @@ const CourseDetail = () => {
               <h1 className="text-lg md:text-xl font-semibold">Course Price</h1>
             </CardContent>
             <CardFooter className="flex justify-center p-4">
-              <Button variant="" className="w-full"	>
-                {purchasedcourse ? "Continue course" : "Purchase Course"}
-              </Button>
+              {purchasedcourse ? (
+                <Button className="w-full">Continue to course</Button>
+              ) : (
+                <BuyCourseButton />
+              )}
             </CardFooter>
           </Card>
         </div>
