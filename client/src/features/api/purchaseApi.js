83db556a5ec2607b7d931d1a40c8a03a7bab7e6d
Yogesh Palve <dnyyogesh@gmail.com>
2025-01-31 10:43:37 +0530

    purchase course api integration

--- client/src/features/api/purchaseApi.js
+++ client/src/features/api/purchaseApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const COURSE_PURCHASE_API = "http://localhost:8080/api/v1/purchase";
 
-const purchaseApi = createApi({
+export const purchaseApi = createApi({
   reducerPath: "purchaseApi",
   baseQuery: fetchBaseQuery({
     baseUrl: COURSE_PURCHASE_API,
