86ffbe641b7a44c0d79b6bb244f4130da3d955f3
Yogesh Palve <dnyyogesh@gmail.com>
2025-02-05 22:39:05 +0530

    search page functionality added

--- client/src/pages/student/HeroSection.jsx
+++ client/src/pages/student/HeroSection.jsx
@@ -1,8 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const HeroSection = () => {
+  const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const searchHandler = (e) => {
+    e.preventDefault();
+    if (searchQuery.trim() !== "") {
+      navigate(`/course/search?query=${searchQuery}`);
+    }
+    setSearchQuery(""); // Optionally reset the search input after navigating
+  };
+
   return (
     <div
       className="relative bg-gradient-to-r from-blue-500 to bg-indigo-600 dark:from-gray-800 dark:to-gray-900
@@ -15,13 +27,21 @@ const HeroSection = () => {
         <p className="text-gray-200 dark:text-gray-400">
           Discover, Learn, and Upskill with our wide range of Courses
         </p>
-        <form className="flex items-center bg-white dark:bg-gray-800 rounded-full shadow-lg overflow-hidden max-w-xl mx-auto mt-7 mb-6">
+        <form
+          onSubmit={searchHandler}
+          className="flex items-center bg-white dark:bg-gray-800 rounded-full shadow-lg overflow-hidden max-w-xl mx-auto mt-7 mb-6"
+        >
           <Input
             type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
             placeholder="Search Courses"
             className="flex-grow border-none focus-visible:ring-0 px-6 py-3 text-gray-900 dark:text-gray-100 placeholder-gray-400 dark:placeholder-gray-500"
           />
-          <Button className="bg-blue-600 dark:bg-blue-800 text-white px-6 py-3 rounded-r-full hover:bg-blue-700">
+          <Button
+            type="submit"
+            className="bg-blue-600 dark:bg-blue-800 text-white px-6 py-3 rounded-r-full hover:bg-blue-700"
+          >
             Search
           </Button>
         </form>
